6d21f172db04a184db36b69657ac45a41d7bd750
eonarheim <erik.onarheim@gmail.com>
2015-12-21 15:21:15 -0600

    Add excalibur specific theme changes for obsolete methods

--- bin/default/theme.js
+++ bin/default/theme.js
@@ -1,1 +1,28 @@
+var Handlebars = require("handlebars");
+
+Handlebars.registerHelper("ifEquals", function( value1, value2, options ){
+	return value1 === value2 ? options.fn(this) : "";
+});
+
+Handlebars.registerHelper("containsTag", function( value, declaration, options ){
+	// fallback...
+	var array = [];
+	if(declaration && declaration.signatures){
+		declaration.signatures.forEach(function(s){
+			if(s.comment && s.comment.tags){
+				array = array.concat(s.comment.tags);
+			}
+		});		
+		
+		//if(array) console.log("contains", value, array);
+	}
+	
+	array = array.map(function(t){
+		if (t) {
+			return t.tagName;
+		}
+		return false;
+	});
+	return (array.indexOf(value) > -1) ? options.fn( this ) : "";
+});
 exports = td.output.DefaultTheme;
